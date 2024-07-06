aba1bd99d563236dfded073ba6c769ff9530ea89
nrdr <81265253+nrdr@users.noreply.github.com>
2024-07-06 19:47:12 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -80,10 +80,6 @@ class CarInterface(CarInterfaceBase):
       if candidate in HONDA_BOSCH_RADARLESS:
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     elif ret.enableGasInterceptor:
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.33, 0.22]
       ret.stoppingDecelRate = 0.2 # avoid harsh braking when entering stopped state
     else:
       # default longitudinal tuning for this branch
