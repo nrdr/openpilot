008f82cd1465ad7eb0f5c59938e8e0f50f8aca7f
Comma Device <81265253+nrdr@users.noreply.github.com>
2024-12-19 15:11:07 -0500

    Comma Pedal Tuning

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -84,10 +84,14 @@ class CarInterface(CarInterfaceBase):
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     else:
       # default longitudinal tuning for all hondas
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      # Soft p:      [1.2, 0.8, 0.5]
+      # Agressive p: [3.6, 2.4, 1.5]
+      # Soft i:      [0.18, 0.12]
+      # Agressive i: [0.54, 0.36]
+      ret.longitudinalTuning.kpBP = [0., 5., 22., 29.]
+      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5, 1.4]
+      ret.longitudinalTuning.kiBP = [0., 22., 29.]
+      ret.longitudinalTuning.kiV = [0.18, 0.12, 0.32]
 
     eps_modified = False
     for fw in car_fw:
