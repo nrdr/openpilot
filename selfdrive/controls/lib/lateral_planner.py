a3e46e23f15b8be20c760440d2f4f63f8270d5c0
Comma Device <81265253+nrdr@users.noreply.github.com>
2024-12-25 01:00:44 -0500

    This should feel a bit better for my civic

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -117,9 +117,9 @@ class LateralPlanner:
 
     if self.v_ego > 11:
       # boost steerRatio by boost amount if desired steer angle is high
-      self.steerRatio = 16.93
+      self.steerRatio = 14.10
     elif self.v_ego > 14:
-      self.steerRatio = 19.93
+      self.steerRatio = 16.50
     else:
       self.steerRatio = 10.93
 
