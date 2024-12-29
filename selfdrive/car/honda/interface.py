f8f5089e50c812d9f391e3d6c046d043c5779a21
Comma Device <81265253+nrdr@users.noreply.github.com>
2024-12-28 21:44:20 -0500

    try blue, its the new red

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -88,9 +88,9 @@ class CarInterface(CarInterfaceBase):
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     elif soft_tune:
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.5, 1.2, 1.0]
+      ret.longitudinalTuning.kpV = [2.4, 1.6, 0.8]
       ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kiV = [0.20, 0.16]
       ret.vEgoStopping = 0.15
       ret.vEgoStarting = 0.15
       ret.stopAccel = -0.4
