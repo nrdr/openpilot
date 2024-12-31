58a94f48b0d4d4bade4381586f8b2ba6b2376571
Comma Device <81265253+nrdr@users.noreply.github.com>
2024-12-30 19:38:20 -0500

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -86,6 +86,8 @@ class CarInterface(CarInterfaceBase):
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
 
     if ret.enableGasInterceptorDEPRECATED:
+      ret.longitudinalTuning.deadzoneBP = [0., 8. 21., 22.]
+      ret.longitudinalTuning.deadzoneV = [.0, .14, .19, 0.]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
@@ -93,7 +95,7 @@ class CarInterface(CarInterfaceBase):
       ret.vEgoStopping = 0.1
       ret.vEgoStarting = 0.5
       ret.stopAccel = -2.0
-      ret.stoppingDecelRate = 0.8
+      ret.stoppingDecelRate = 0.4
       ret.longitudinalActuatorDelayLowerBound = 0.15
       ret.longitudinalActuatorDelayUpperBound = 0.15
 
