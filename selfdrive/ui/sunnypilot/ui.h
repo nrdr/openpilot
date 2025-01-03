32241d1d6b5ef4ad454172140f1f50dc15dda36e
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-03 13:13:35 -0500

    Final colors?

--- selfdrive/ui/sunnypilot/ui.h
+++ selfdrive/ui/sunnypilot/ui.h
@@ -58,8 +58,8 @@ struct FeatureStatusColor {
 const QColor sp_bg_colors [] = {
   [STATUS_DISENGAGED] = bg_colors[STATUS_DISENGAGED],
   [STATUS_OVERRIDE] = bg_colors[STATUS_OVERRIDE],
-  [STATUS_ENGAGED] = QColor(0x14, 0x00, 0xE8, 0xf1),
-  [STATUS_MADS] = QColor(0x14, 0x00, 0xE8, 0xf1),
+  [STATUS_ENGAGED] = QColor(0x14, 0x8C, 0xE8, 0xf1), // blue
+  [STATUS_MADS] = QColor(0x14, 0x8C, 0xE8, 0xf1), // blue
 };
 #define bg_colors sp_bg_colors // Override the bg_colors array with the sp_bg_colors array
 
