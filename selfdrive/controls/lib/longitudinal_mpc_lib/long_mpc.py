ddb9b75c982248c049ed90f8e38cddabf343542a
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-11 01:59:55 -0500

    Actually drive at a reasonable pace

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,15 +74,15 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    return 1.75
+    return 2.0
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.45
+    return 1.5
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.25
-  elif personality==custom.LongitudinalPersonalitySP.aggressive:
     return 1.0
+  elif personality==custom.LongitudinalPersonalitySP.aggressive:
+    return 0.5
   elif personality==custom.LongitudinalPersonalitySP.overtake:
-    return 0.25
+    return 0.1
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
