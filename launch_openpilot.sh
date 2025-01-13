3a920fafd71a34f224c6ef4cc902e62e759c9874
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-13 17:16:33 -0500

    Reapply "SR/Switch between stock openpilot method and custom"
    This reverts commit cd42ce2aac703df4dadda5771025d05d17608ed0.

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
-# I find the car drive the best after a fresh LiveParameters deletion, from straights to curves
-while true; do rm -rf /data/params/d/LiveParameters; sleep 30; done &
-
 exec ./launch_chffrplus.sh
