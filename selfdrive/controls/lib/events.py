26bcb0c11639a4ce2a062b71fc1e54a698a85141
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-25 17:07:47 -0500

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -943,9 +943,11 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
   },
 
   EventName.accFaulted: {
-    ET.IMMEDIATE_DISABLE: ImmediateDisableAlert("Cruise Fault: Restart the Car"),
-    ET.PERMANENT: NormalPermanentAlert("Cruise Fault: Restart the car to engage"),
-    ET.NO_ENTRY: NoEntryAlert("Cruise Fault: Restart the Car"),
+    ET.WARNING: Alert(
+      "Openpilot could not block stock system in time!",
+      "This may or may not cause problems. Use at your own risk.",
+      AlertStatus.normal, AlertSize.mid,
+      Priority.LOW, VisualAlert.none, AudibleAlert.none, 1.),
   },
 
   EventName.espActive: {
