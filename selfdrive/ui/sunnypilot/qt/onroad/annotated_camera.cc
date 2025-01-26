e088de39e3140fa37bc96d74b4375ac1f4ea4f1a
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-26 02:45:08 -0500

    Update annotated_camera.cc

--- selfdrive/ui/sunnypilot/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/sunnypilot/qt/onroad/annotated_camera.cc
@@ -120,7 +120,7 @@ void AnnotatedCameraWidgetSP::updateButtonsLayout(bool is_rhd) {
     buttons_layout->addWidget(onroad_settings_btn, 0, Qt::AlignBottom | Qt::AlignRight);
     buttons_layout->addSpacing(onroad_settings_btn->isVisible() ? 216 : 0);
   } else {
-    buttons_layout->addSpacing(onroad_settings_btn->isVisible() ? 216 : 0);
+    buttons_layout->addSpacing(onroad_settings_btn->isVisible() ? 0 : 0);
     buttons_layout->addWidget(onroad_settings_btn, 0, Qt::AlignBottom | Qt::AlignLeft);
 
     buttons_layout->addStretch(1);
