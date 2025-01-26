3b7cd069354cf88613e7f80ff0dde729fba49f90
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-26 00:40:04 -0500

    Update annotated_camera.cc

--- selfdrive/ui/sunnypilot/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/sunnypilot/qt/onroad/annotated_camera.cc
@@ -827,7 +827,7 @@ void AnnotatedCameraWidgetSP::drawNewDevUi2(QPainter &p, int x, int y) {
   int rw = 300; // compile
 
   UiElement actualLateralAccelElement = DeveloperUi::getActualLateralAccel(curvature, vEgo, roll, madsEnabled, latActive);
-  rw += drawDevUiRight(p, rw, y, actualLateralAccelElement.value, actualLateralAccelElement.label, actualLateralAccelElement.units, actualLateralAccelElement.color);
+  rw += drawNewDevUi(p, rw, y, actualLateralAccelElement.value, actualLateralAccelElement.label, actualLateralAccelElement.units, actualLateralAccelElement.color);
 
   UiElement latAccelFactorFilteredElement = DeveloperUi::getLatAccelFactorFiltered(latAccelFactorFiltered, liveValid);
   rw += drawNewDevUi(p, rw, y, latAccelFactorFilteredElement.value, latAccelFactorFilteredElement.label, latAccelFactorFilteredElement.units, latAccelFactorFilteredElement.color);
