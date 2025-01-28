6239441d84b40655412b691bec9df7fa791c1d70
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-28 17:55:34 -0500

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -748,6 +748,7 @@ class Controls:
       if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents):
         if not self.experimental_mode_update:
           self.personality = (self.personality + 1) % 4
+          self.accel_personality = (self.accel_personality + 1) % 4
           self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
         self.experimental_mode_update = False
 
