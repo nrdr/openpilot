2fd640d49e87c716e22ed454b67fec54f8a5e26c
nrdr <81265253+nrdr@users.noreply.github.com>
2025-01-28 21:26:16 -0500

    Revert "Correct Settings for Modded Civic EPS"
    This reverts commit d718d76f2c3c871d56d7e242a3a2f96c6619eac7.

--- selfdrive/ui/sunnypilot/qt/offroad/settings/sunnypilot_settings.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/sunnypilot_settings.cc
@@ -111,7 +111,7 @@ SunnypilotPanel::SunnypilotPanel(QWidget *parent) : QFrame(parent) {
     },
     {
       "TorqueLateralJerk",
-      tr("NNLC Controller Lite"),
+      tr("Lateral Jerk with Torque Lateral Control (Alpha)"),
       tr("Utilizes limited lateral jerk control for improved steering response, leveraging stock torque lateral controller capabilities. Designed to mimic NNLC behavior without training models or data collection."),
       "../assets/offroad/icon_blank.png",
     },
