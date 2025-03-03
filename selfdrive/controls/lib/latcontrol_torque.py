051f2d2e6f182d1f2111f5f1fa81f79e7b56a8e1
nrdr <81265253+nrdr@users.noreply.github.com>
2025-03-03 18:14:39 -0500

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -25,7 +25,7 @@ from openpilot.selfdrive.modeld.constants import ModelConstants
 # move it at all, this is compensated for too.
 
 LOW_SPEED_X = [0, 10, 20, 30]
-LOW_SPEED_Y = [30, 17, 10, 0]
+LOW_SPEED_Y = [75, 17, 10, 0]
 LOW_SPEED_Y_NN = [12, 3, 1, 0]
 
 # Full friction at or below ||0.6 m/s^2||, no friction at or above ||0.8 m/s^2||
