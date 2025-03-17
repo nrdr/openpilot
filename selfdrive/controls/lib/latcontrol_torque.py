2c5d1a608f5cd26a821f92214d5d36c40a509d61
nrdr <81265253+nrdr@users.noreply.github.com>
2025-03-17 13:42:19 -0400

    Updated Dynamic Friction Test

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -28,9 +28,9 @@ LOW_SPEED_X = [0, 10, 20, 30]
 LOW_SPEED_Y = [75, 17, 10, 0]
 LOW_SPEED_Y_NN = [12, 3, 1, 0]
 
-# Full friction at or below ||0.6 m/s^2||, no friction at or above ||0.8 m/s^2||
-FRICTION_X = [0.4, 0.6] # m/s^2 lateral acceleration
-FRICTION_Y = [1.0, 0.5]
+# Tim's Default: Full friction at or below ||0.6 m/s^2||, no friction at or above ||0.8 m/s^2||
+FRICTION_X = [0.3, 0.6, 0.9] # m/s^2 lateral acceleration
+FRICTION_Y = [1.0, 0.5, 0.2]
 
 LAT_PLAN_MIN_IDX = 5
 
