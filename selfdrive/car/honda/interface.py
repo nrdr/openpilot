5870fa9c88faa4406d54a5134453d5e7af06db17
nrdr <81265253+nrdr@users.noreply.github.com>
2025-03-26 15:10:46 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -111,7 +111,7 @@ class CarInterface(CarInterfaceBase):
       # ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
       # honda values noted above
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      ret.longitudinalTuning.kiV = [1.2, 1.4, 1.6]
 
     eps_modified = False
     for fw in car_fw:
