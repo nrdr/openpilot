f60ff160d5c2c5390d671740c8a8bd2989885de3
nrdr <81265253+nrdr@users.noreply.github.com>
2025-04-23 12:36:05 -0400

    Update lateral_planner.py

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -177,7 +177,8 @@ class LateralPlanner:
     if not self.model_use_lateral_planner:
       self.road_edge = get_road_edge(sm['carState'], md, self.edge_toggle)
 
-  def get_dynamic_lane_profile(self, longitudinal_plan_sp):
+  def get_dynamic_lane_profile(self, longitudinal_plan_sp, sm):
+    v_ego_car = sm['carState'].vEgo
     if self.dynamic_lane_profile == 1:
       return True
     elif self.dynamic_lane_profile == 0:
