e1c643a3ad45eb23de668190543737d98253d797
nrdr <81265253+nrdr@users.noreply.github.com>
2025-04-26 15:35:40 -0400

    Update carcontroller.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -102,14 +102,14 @@ HUDData = namedtuple("HUDData",
 
 def rate_limit_steer(new_steer, last_steer, speed):
   # Define the maximum delta at higher speeds
-  max_delta = 2 * DT_CTRL
+  max_delta = 3 * DT_CTRL
   
   # Define the speed threshold (25 MPH in m/s)
   speed_threshold = 50 * CV.MPH_TO_MS
 
   # Adjust MAX_DELTA based on speed, reducing it as speed drops below the threshold
   if speed < speed_threshold:
-    reduction_factor = 0.65
+    reduction_factor = 1
     max_delta -= reduction_factor * DT_CTRL
 
   return clip(new_steer, last_steer - max_delta, last_steer + max_delta)
