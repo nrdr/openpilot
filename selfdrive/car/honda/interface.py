677effe06e94f6bb599e2d6298c414527b2a83fc
nrdr <81265253+nrdr@users.noreply.github.com>
2025-04-29 09:59:53 -0400

    Extra Coasting

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -27,10 +27,7 @@ class CarInterface(CarInterfaceBase):
     if CP.carFingerprint in HONDA_BOSCH:
       return CarControllerParams.BOSCH_ACCEL_MIN, CarControllerParams.BOSCH_ACCEL_MAX
     elif CP.enableGasInterceptorDEPRECATED:
-      # Alt approach to long where car gently stops accel near the set speed for comfort
-      ACCEL_MAX_VALS = [CarControllerParams.NIDEC_ACCEL_MAX, 0.3, 0.0]
-      ACCEL_MAX_BP = [cruise_speed - 4., cruise_speed - .2, cruise_speed + 1.]
-      return CarControllerParams.NIDEC_ACCEL_MIN, interp(current_speed, ACCEL_MAX_BP, ACCEL_MAX_VALS)
+      return CarControllerParams.NIDEC_ACCEL_MIN, CarControllerParams.NIDEC_ACCEL_MAX
     else:
       # NIDECs don't allow acceleration near cruise_speed,
       # so limit limits of pid to prevent windup
