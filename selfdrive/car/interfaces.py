f2c33152b68c3e330f1720f8c23070eb52ad8a25
nrdr <81265253+nrdr@users.noreply.github.com>
2025-05-15 22:08:45 -0400

    Good so far. Slight sway. Try boost

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -392,9 +392,9 @@ class CarInterfaceBase(ABC):
 
     tune.init('torque')
     tune.torque.useSteeringAngle = use_steering_angle
-    tune.torque.kp = 0.5
-    tune.torque.kf = 0.5
-    tune.torque.ki = 0.05
+    tune.torque.kp = 0.7
+    tune.torque.kf = 0.7
+    tune.torque.ki = 0.07
     tune.torque.friction = params['FRICTION']
     tune.torque.latAccelFactor = params['LAT_ACCEL_FACTOR']
     tune.torque.latAccelOffset = 0.0
