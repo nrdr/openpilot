b9f3ff31fdd4b67a49d622f0d3f937a22bff0fb8
nrdr <81265253+nrdr@users.noreply.github.com>
2025-05-20 12:18:27 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -131,7 +131,7 @@ class CarInterface(CarInterfaceBase):
         # ret.lateralParams.torqueV =   [0x0, 0x200, 0x300, 0x478, 0x5EC, 0x800, 0xA00, 0xE00, 0xF00] (hexdecimal)
         # ret.lateralParams.torqueBP = [0, 2327, 3525, 4119, 4511, 5131, 5760, 22464, 25344]
         # ret.lateralParams.torqueV = [0, 512, 768, 1144, 1516, 2048, 2560, 3584, 3840]
-        ret.lateralParams.torqueBP = [0, 2560, 50000]
+        ret.lateralParams.torqueBP = [0, 51200, 65800]
         ret.lateralParams.torqueV = [0, 2560, 3840]
         ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 4.4704, 13.4112, 20.1168], [0.3, 0.3, 0.3, 0.3]]   # 0 / 10 / 30 / 45 MPH
         ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 4.4704, 13.4112, 20.1168], [0.1, 0.1, 0.1, 0.1]]  # 0 / 10 / 30 / 45 MPH
