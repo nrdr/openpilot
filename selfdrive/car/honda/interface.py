a93563eb54992f71831048f1536f5fbf4ed170dc
nrdr <81265253+nrdr@users.noreply.github.com>
2025-05-27 10:18:44 -0400

    Test alt lateral control method

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -37,9 +37,9 @@ class CarInterface(CarInterfaceBase):
       return CarControllerParams.NIDEC_ACCEL_MIN, interp(current_speed, ACCEL_MAX_BP, ACCEL_MAX_VALS)
 
   def torque_from_lateral_accel_modded(self, latcontrol_inputs: LatControlInputs, torque_params: car.CarParams.LateralTorqueTuning, lateral_accel_error: float, lateral_accel_deadzone: float, friction_compensation: bool, gravity_adjusted: bool) -> float:
-    threshold = 0.9
+    threshold = 0.8
     threshold_lat_accel = 1/torque_params.latAccelFactor * threshold
-    mod_factor = 2.0 # Lateral Accel
+    mod_factor = 4.4 # Lateral Accel
     # The default is a linear relationship between torque and lateral acceleration (accounting for road roll and steering friction)
     friction = get_friction(lateral_accel_error, lateral_accel_deadzone, FRICTION_THRESHOLD, torque_params, friction_compensation)
     if abs(latcontrol_inputs.lateral_acceleration) > threshold_lat_accel:
@@ -132,8 +132,8 @@ class CarInterface(CarInterfaceBase):
         # ret.lateralParams.torqueBP = [0, 2327, 3525, 4119, 4511, 5131, 5760, 22464, 25344]
         # ret.lateralParams.torqueV = [0, 512, 768, 1144, 1516, 2048, 2560, 3584, 3840]
         ret.lateralTuning.pid.kf = 0.00006
-        ret.lateralParams.torqueBP = [0, 2327, 3525, 4119, 4511, 5131, 5760, 22464, 25344]
-        ret.lateralParams.torqueV = [0, 512, 768, 1144, 1516, 2048, 2560, 3584, 3840]
+        ret.lateralParams.torqueBP = [0, 2560, 3840]
+        ret.lateralParams.torqueV = [0, 2560, 3840]
         ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 4.4704, 13.4112, 20.1168], [0.3, 0.3, 0.3, 0.3]]   # 0 / 10 / 30 / 45 MPH
         ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 4.4704, 13.4112, 20.1168], [0.1, 0.1, 0.1, 0.1]]  # 0 / 10 / 30 / 45 MPH
       else:
