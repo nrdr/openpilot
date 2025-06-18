bddc028340ccae2bca56aaee4ab2f63c26a608a2
nrdr <81265253+nrdr@users.noreply.github.com>
2025-06-18 18:36:21 -0500

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -52,7 +52,7 @@ class CarInterface(CarInterfaceBase):
     return torque + friction
 
   def torque_from_lateral_accel(self) -> TorqueFromLateralAccelCallbackType:
-    if self.CP.enableGasInterceptorDEPRECATED:
+    if not self.CP.enableGasInterceptorDEPRECATED:
       return self.torque_from_lateral_accel_modded
     else:
       return self.torque_from_lateral_accel_linear
