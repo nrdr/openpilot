4a5226d02900951c4877904e948bbaa1f0966de2
THERoen <199319559+THERoen@users.noreply.github.com>
2025-11-02 22:34:34 -0500

    No positive P-response for long control if user-selected parameter set
    Integrated twilsonco commit https://github.com/twilsonco/openpilot/commit/fcdcba66ad20d294d794135463f25d85092b4162
    
    Co-Authored-By: Tim Wilson <7284371+twilsonco@users.noreply.github.com>

--- common/pid.py
+++ common/pid.py
@@ -2,7 +2,9 @@ import numpy as np
 from numbers import Number
 
 class PIDController:
-  def __init__(self, k_p, k_i, k_f=0., k_d=0., pos_limit=1e308, neg_limit=-1e308, rate=100):
+  def __init__(self, k_p, k_i, k_f=0., k_d=0.,
+              pos_limit=1e308, neg_limit=-1e308, rate=100,
+              pos_p_limit=None, neg_p_limit=None):
     self._k_p = k_p
     self._k_i = k_i
     self._k_d = k_d
@@ -15,6 +17,9 @@ class PIDController:
       self._k_d = [[0], [self._k_d]]
 
     self.set_limits(pos_limit, neg_limit)
+
+    self.pos_p_limit = pos_p_limit
+    self.neg_p_limit = neg_p_limit
 
     self.i_rate = 1.0 / rate
     self.speed = 0.0
@@ -47,6 +52,11 @@ class PIDController:
   def update(self, error, error_rate=0.0, speed=0.0, feedforward=0., freeze_integrator=False):
     self.speed = speed
     self.p = float(error) * self.k_p
+    if self.pos_p_limit is not None and self.p > self.pos_p_limit:
+      self.p = self.pos_p_limit
+    elif self.neg_p_limit is not None and self.p < self.neg_p_limit:
+      self.p = self.neg_p_limit
+
     self.f = feedforward * self.k_f
     self.d = error_rate * self.k_d
 
--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -52,9 +52,11 @@ class LongControl:
     self.CP = CP
     self.CP_SP = CP_SP
     self.long_control_state = LongCtrlState.off
+    pos_p_limit = 0.0 # if params("NoPositivePResponse") else None # put parameter-based control here
     self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                              (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
-                             k_f=CP.longitudinalTuning.kf, rate=1 / DT_CTRL)
+                             k_f=CP.longitudinalTuning.kf, rate=1 / DT_CTRL,
+                             pos_p_limit=pos_p_limit)
     self.last_output_accel = 0.0
 
   def reset(self):
