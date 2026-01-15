828010ff9b4857b18834611269a576e042fec37d
nrdr <81265253+nrdr@users.noreply.github.com>
2026-01-15 12:03:22 -0500

    Revert "Speed should always be in whole numbers!"
    This reverts commit 3c6aa9232029beb00afb12748b4bbc870c2d276a.

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from cereal import car
 from openpilot.common.constants import CV
-from openpilot.common.params import Params
 from openpilot.sunnypilot.selfdrive.car.cruise_ext import VCruiseHelperSP
 
 
@@ -15,6 +14,7 @@ V_CRUISE_MAX = 145
 V_CRUISE_UNSET = 255
 V_CRUISE_INITIAL = 40
 V_CRUISE_INITIAL_EXPERIMENTAL_MODE = 105
+IMPERIAL_INCREMENT = round(CV.MPH_TO_KPH, 1)  # round here to avoid rounding errors incrementing set speed
 
 ButtonEvent = car.CarState.ButtonEvent
 ButtonType = car.CarState.ButtonEvent.Type
@@ -33,9 +33,6 @@ class VCruiseHelper(VCruiseHelperSP):
   def __init__(self, CP, CP_SP):
     VCruiseHelperSP.__init__(self, CP, CP_SP)
     self.CP = CP
-    self.CP_SP = CP_SP
-    self.params = Params()
-
     self.v_cruise_kph = V_CRUISE_UNSET
     self.v_cruise_cluster_kph = V_CRUISE_UNSET
     self.v_cruise_kph_last = 0
@@ -60,7 +57,6 @@ class VCruiseHelper(VCruiseHelperSP):
         self.v_cruise_cluster_kph = self.v_cruise_kph
         self.update_button_timers(CS, enabled)
       else:
-        # PCM-based set speed (this comes from the car; don't quantize here)
         self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
         self.v_cruise_cluster_kph = CS.cruiseState.speedCluster * CV.MS_TO_KPH
         if CS.cruiseState.speed == 0:
@@ -81,6 +77,8 @@ class VCruiseHelper(VCruiseHelperSP):
 
     long_press = False
     button_type = None
+
+    v_cruise_delta = 1. if is_metric else IMPERIAL_INCREMENT
 
     for b in CS.buttonEvents:
       if b.type.raw in self.button_timers and not b.pressed:
@@ -113,50 +111,17 @@ class VCruiseHelper(VCruiseHelperSP):
     if self.update_speed_limit_assist_pre_active_confirmed(button_type):
       return
 
-    # -------------------------------------------------------------------------
-    # KEY CHANGE:
-    # Work in DISPLAY UNITS:
-    # - metric: integer kph
-    # - imperial: integer mph (always), then convert back to kph for storage
-    # This prevents fractional mph (e.g., 53.7 mph) from ever being produced.
-    # -------------------------------------------------------------------------
-    if is_metric:
-      v_disp = float(self.v_cruise_kph)                 # kph
-      disp_min = float(self.v_cruise_min)               # kph
-      disp_max = float(V_CRUISE_MAX)                    # kph
-      base_delta = 1.0                                  # 1 kph steps
+    long_press, v_cruise_delta = VCruiseHelperSP.update_v_cruise_delta(self, long_press, v_cruise_delta)
+    if long_press and self.v_cruise_kph % v_cruise_delta != 0:  # partial interval
+      self.v_cruise_kph = CRUISE_NEAREST_FUNC[button_type](self.v_cruise_kph / v_cruise_delta) * v_cruise_delta
     else:
-      v_disp = float(self.v_cruise_kph) * CV.KPH_TO_MPH  # mph
-      disp_min = float(self.v_cruise_min) * CV.KPH_TO_MPH
-      disp_max = float(V_CRUISE_MAX) * CV.KPH_TO_MPH
-      base_delta = 1.0                                   # 1 mph steps
-
-    # Allow VCruiseHelperSP to modify delta for long-press behavior
-    long_press, disp_delta = VCruiseHelperSP.update_v_cruise_delta(self, long_press, base_delta)
-
-    # Snap to nearest interval on long-press if we're on a partial step
-    if long_press and (v_disp % disp_delta) != 0:
-      v_disp = CRUISE_NEAREST_FUNC[button_type](v_disp / disp_delta) * disp_delta
-    else:
-      v_disp += disp_delta * CRUISE_INTERVAL_SIGN[button_type]
+      self.v_cruise_kph += v_cruise_delta * CRUISE_INTERVAL_SIGN[button_type]
 
     # If set is pressed while overriding, clip cruise speed to minimum of vEgo
     if CS.gasPressed and button_type in (ButtonType.decelCruise, ButtonType.setCruise):
-      v_ego_disp = CS.vEgo * (CV.MS_TO_KPH if is_metric else CV.MS_TO_MPH)
-      v_disp = max(v_disp, v_ego_disp)
+      self.v_cruise_kph = max(self.v_cruise_kph, CS.vEgo * CV.MS_TO_KPH)
 
-    # Enforce whole-number display speeds
-    if is_metric:
-      v_disp = float(int(round(v_disp)))  # whole kph
-      v_disp = float(np.clip(v_disp, disp_min, disp_max))
-      self.v_cruise_kph = v_disp
-    else:
-      v_disp = float(int(round(v_disp)))  # whole mph, forever
-      v_disp = float(np.clip(v_disp, disp_min, disp_max))
-      self.v_cruise_kph = v_disp * CV.MPH_TO_KPH
-
-    # Safety clip (no 0.1 kph rounding anymore)
-    self.v_cruise_kph = float(np.clip(self.v_cruise_kph, self.v_cruise_min, V_CRUISE_MAX))
+    self.v_cruise_kph = np.clip(round(self.v_cruise_kph, 1), self.v_cruise_min, V_CRUISE_MAX)
 
   def update_button_timers(self, CS, enabled):
     # increment timer for buttons still pressed
@@ -178,21 +143,9 @@ class VCruiseHelper(VCruiseHelperSP):
     initial_experimental_mode = experimental_mode and not dynamic_experimental_control
     initial = V_CRUISE_INITIAL_EXPERIMENTAL_MODE if initial_experimental_mode else V_CRUISE_INITIAL
 
-    is_metric = self.params.get_bool("IsMetric")
-
     if any(b.type in (ButtonType.accelCruise, ButtonType.resumeCruise) for b in CS.buttonEvents) and self.v_cruise_initialized:
       self.v_cruise_kph = self.v_cruise_kph_last
     else:
-      # Start from current speed, but:
-      # - Metric: floor to whole kph
-      # - Imperial: floor to whole mph (round down), then convert back to kph
-      v_kph = float(np.clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX))
-
-      if is_metric:
-        self.v_cruise_kph = float(int(math.floor(v_kph)))
-      else:
-        v_mph = v_kph * CV.KPH_TO_MPH
-        v_mph_floor = float(int(math.floor(v_mph)))  # <-- round DOWN in mph
-        self.v_cruise_kph = v_mph_floor * CV.MPH_TO_KPH
+      self.v_cruise_kph = int(round(np.clip(CS.vEgo * CV.MS_TO_KPH, initial, V_CRUISE_MAX)))
 
     self.v_cruise_cluster_kph = self.v_cruise_kph
