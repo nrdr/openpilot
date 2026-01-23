57d5202e31a04be9fd502f0d56f16bd5db7049ca
MVL <manuel@alumni.virginia.edu>
2026-01-22 22:18:57 -0500

    don't need Hardware w mount override

--- openpilot/selfdrive/locationd/calibrationd.py
+++ openpilot/selfdrive/locationd/calibrationd.py
@@ -14,7 +14,7 @@ from typing import NoReturn
 from openpilot.cereal import log
 from opendbc.car.structs import car
 import openpilot.cereal.messaging as messaging
-from openpilot.common.hardware import HARDWARE
+# from openpilot.common.hardware import HARDWARE
 from openpilot.common.constants import CV
 from openpilot.common.params import Params
 from openpilot.common.realtime import config_realtime_process
