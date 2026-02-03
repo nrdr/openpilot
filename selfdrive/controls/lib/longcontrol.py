5790387dd7dc1d1be3347196fb7876d80a4f6a67
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-03 07:28:22 -0500

    Enable live PID

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -57,7 +57,7 @@ class LongControl:
     else:
       return self.pid_standard
 
-  def update(self, active, CS, a_target, should_stop, accel_limits, personality=None):
+  def update(self, active, CS, a_target, should_stop, accel_limits, personality):
     """Update longitudinal control. This updates the state machine and runs a PID loop"""
 
     # --- live personality PID switching ---
