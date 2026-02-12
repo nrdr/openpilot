87f1f75117a613a3ab2349b4fa7a8923b8a8fa8f
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-12 15:03:35 -0500

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -14,7 +14,7 @@ from openpilot.sunnypilot.models.helpers import get_active_model_runner
 from openpilot.sunnypilot.sunnylink.utils import sunnylink_need_register, sunnylink_ready, use_sunnylink_uploader
 
 WEBCAM = os.getenv("USE_WEBCAM") is not None
-DISABLE_DM = True
+DISABLE_DM = False
 
 def driverview(started: bool, params: Params, CP: car.CarParams) -> bool:
   return started or params.get_bool("IsDriverViewEnabled")
