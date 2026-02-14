89bc85ddd31ae42d703f1fd4249d7dc8293e906f
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-13 23:54:41 -0500

    Reduced PID Tune

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -374,16 +374,16 @@ class CarInterface(CarInterfaceBase):
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
         for fw in car_fw:
           if fw.ecu == "eps" and b"-" not in fw.fwVersion and b"," in fw.fwVersion:
-            stock_cp.lateralTuning.pid.kf = 0.00002
+            stock_cp.lateralTuning.pid.kf = 0.00001
             # 5210 max stable 1x stock
             # 10240 max stable 2x request
             # 32767 max EVER. Really powerful
             # 0, 2184, 4369, 6553, 8738, 13107, 17476, 21845, 32767 # current table
-            stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2400, 3200, 3840], [0, 611, 1800, 3840]]
-            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.07875], [0.025875]]
+            stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
+            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.0393], [0.125]]
           elif fw.ecu == "eps" and b"-" in fw.fwVersion and b"," in fw.fwVersion:
-            stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2400, 3200, 3840], [0, 611, 1800, 3840]]
-            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.07875], [0.025875]]
+            stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
+            stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.0393], [0.125]]
       else:
         stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2560], [0, 2560]]
         stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.8], [0.24]]
