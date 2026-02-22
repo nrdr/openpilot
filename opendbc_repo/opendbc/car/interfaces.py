8a669348f8dcf7f783391fddd7fe6881af712d17
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-22 12:27:50 -0500

    🕶️ Night Rider Goodies Package

--- opendbc_repo/opendbc/car/interfaces.py
+++ opendbc_repo/opendbc/car/interfaces.py
@@ -245,9 +245,9 @@ class CarInterfaceBase(ABC, CarInterfaceBaseSP):
     ret.steerRatioRear = 0.  # no rear steering, at least on the listed cars aboveA
     ret.openpilotLongitudinalControl = False
     ret.stopAccel = -2.0
-    ret.stoppingDecelRate = 0.8 # brake_travel/s while trying to stop
-    ret.vEgoStopping = 0.5
-    ret.vEgoStarting = 0.5
+    ret.stoppingDecelRate = 0.2 # brake_travel/s while trying to stop
+    ret.vEgoStopping = 2.0
+    ret.vEgoStarting = 2.0
     ret.longitudinalTuning.kpBP = [0.]
     ret.longitudinalTuning.kpV = [0.]
     ret.longitudinalTuning.kiBP = [0.]
