3d339dbcb53ff4ec9d087574a76343b3000c824d
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-22 12:27:50 -0500

    🕶️ Night Rider Goodies Package

--- panda/board/drivers/can_common.h
+++ panda/board/drivers/can_common.h
@@ -202,7 +202,7 @@ void ignition_can_hook(CANPacket_t *msg) {
     }
   }
 
-  // body v2 exception
+  // body exception
   if (((msg->bus == 0U) || (msg->bus == 2U)) && (msg->addr == 0x222U)) {
     ignition_can = true;
     ignition_can_cnt = 0U;
