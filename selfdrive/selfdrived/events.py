e3f1540687ed8ed3cff36b89a6fe1865b03ea035
nrdr <81265253+nrdr@users.noreply.github.com>
2026-02-23 14:54:48 -0500

    Revert "Update events.py"
    This reverts commit e43e824c2d2c9c2376bcb00cdb310e6b383d4e40.

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -672,8 +672,9 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
   # is thrown. This can mean a service crashed, did not broadcast a message for
   # ten times the regular interval, or the average interval is more than 10% too high.
   EventName.commIssue: {
+    ET.SOFT_DISABLE: soft_disable_alert("Communication Issue Between Processes"),
+    ET.NO_ENTRY: comm_issue_alert,
   },
-  
   EventName.commIssueAvgFreq: {
     ET.SOFT_DISABLE: soft_disable_alert("Low Communication Rate Between Processes"),
     ET.NO_ENTRY: NoEntryAlert("Low Communication Rate Between Processes"),
