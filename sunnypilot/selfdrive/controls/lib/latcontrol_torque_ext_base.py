c2ab828e9f217c2eff949216aa88c2a01dd4be34
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-22 20:45:52 -0400

    Intersection Unwind Improvement Test
    -Adds additional lookahead time at low vehicle speeds via a reshaped friction_look_ahead_v/bp curve
    -Allows the controller to recognize upcoming zero-crossings in lateral acceleration earlier
    -Improves steering response during turn exits and direction reversals (e.g., intersections, tight city turns)
    -Maintains original lookahead behavior at ≥ ~20 mph to avoid impacting highway stability and feel

--- sunnypilot/selfdrive/controls/lib/latcontrol_torque_ext_base.py
+++ sunnypilot/selfdrive/controls/lib/latcontrol_torque_ext_base.py
@@ -83,10 +83,14 @@ class LatControlTorqueExtBase:
     # Note that LAT_PLAN_MIN_IDX is defined above and is used in order to prevent
     # using a "future" value that is actually planned to occur before the "current" desired
     # value, which is offset by the steerActuatorDelay.
-    # TODO-SP: Reevaluate lookahead v values that determines how low a desired lateral jerk signal needs to
+    # TODO-SP: Reevaluate lookahead values that determine how low a desired lateral jerk signal needs to
     #          persist in order to be used.
-    self.friction_look_ahead_v = [1.4, 2.0]  # how many seconds in the future to look ahead in [0, ~2.1] in 0.1 increments
-    self.friction_look_ahead_bp = [9.0, 30.0]  # corresponding speeds in m/s in [0, ~40] in 1.0 increments
+    #
+    # The low-speed region is intentionally given additional lookahead time to improve
+    # anticipation of intersection unwind and direction reversals, while preserving the
+    # existing behavior near and above 20 mph.
+    self.friction_look_ahead_bp = [0.0, 6.0, 9.0, 30.0]   # speeds in m/s
+    self.friction_look_ahead_v = [2.6, 2.2, 1.4, 2.0]    # seconds in the future to look ahead
 
     # Scaling the lateral acceleration "friction response" could be helpful for some.
     # Increase for a stronger response, decrease for a weaker response.
