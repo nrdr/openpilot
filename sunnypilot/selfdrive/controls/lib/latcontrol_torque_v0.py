f523e8caf59768daf685842c83181723aca7e573
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-25 22:24:18 -0400

    Update latcontrol_torque_v0.py

--- sunnypilot/selfdrive/controls/lib/latcontrol_torque_v0.py
+++ sunnypilot/selfdrive/controls/lib/latcontrol_torque_v0.py
@@ -12,7 +12,7 @@ from openpilot.selfdrive.controls.lib.latcontrol import LatControl
 
 from openpilot.sunnypilot.selfdrive.controls.lib.latcontrol_torque_ext import LatControlTorqueExt
 
-KP = 0.5
+KP = 0.4
 KF = 0.5
 KI = 0.1
 KD = 0.0
