a78c6eee952c53125c97c7305dbb8904a17dd6da
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-29 21:54:57 -0400

    Fix param bug

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -332,20 +332,17 @@ class CarInterface(CarInterfaceBase):
     if candidate == CAR.HONDA_CIVIC:
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
         stock_cp.steerActuatorDelay = 0.6
-        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
-        stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
+        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]] # TODO: Verify this is stable
         stock_cp.lateralTuning.pid.kf = 0.00006
+        stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
 
     elif candidate in (CAR.HONDA_CIVIC_BOSCH, CAR.HONDA_CIVIC_BOSCH_DIESEL):
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
         stock_cp.steerActuatorDelay = 0.6
-        stock_cp.lateralTuning.init('pid')
+        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]] # TODO: Verify this is stable
         stock_cp.lateralTuning.pid.kf = 0.00006
-        stock_cp.lateralTuning.pid.kpV = [0.3]
-        stock_cp.lateralTuning.pid.kiV = [0.1]
-        stock_cp.lateralParams.torqueBP = [0, 3840]
-        stock_cp.lateralParams.torqueV = [0, 3840]
-      
+        stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
+
     elif candidate == CAR.HONDA_CIVIC_2022:
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
         stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
@@ -367,12 +364,9 @@ class CarInterface(CarInterfaceBase):
        stock_cp.autoResumeSng = True
        stock_cp.minEnableSpeed = -1
        stock_cp.steerActuatorDelay = 0.6
-       stock_cp.lateralTuning.init('pid')
+       stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]] # TODO: Verify this is stable
        stock_cp.lateralTuning.pid.kf = 0.00006
-       stock_cp.lateralTuning.pid.kpV = [0.3]
-       stock_cp.lateralTuning.pid.kiV = [0.1]
-       stock_cp.lateralParams.torqueBP = [0, 3840]
-       stock_cp.lateralParams.torqueV = [0, 3840]
+       stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
 
     elif candidate in (CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_MMR):  # source mlocoteta
       stock_cp.autoResumeSng = True
