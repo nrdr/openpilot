4bfbf82189f32fe604cc5cbd234cd133cd9d7b28
nrdr <81265253+nrdr@users.noreply.github.com>
2026-04-30 09:51:57 -0400

    This wasn't good

--- opendbc_repo/opendbc/car/honda/carcontroller.py
+++ opendbc_repo/opendbc/car/honda/carcontroller.py
@@ -106,25 +106,25 @@ def _torque_lpf_tau(torque_cmd: float, prev_torque_cmd: float, v_ego: float) -> 
 
   if highway:
     if sign_change and torque_delta > 0.15:
-      return 0.1
-    return 0.15
+      return 0.05
+    return 0.1
 
   if sign_change:
     if torque_delta > 0.15:
-      return 0.04
+      return 0.02
     elif torque_delta > 0.05:
-      return 0.1
+      return 0.05
     else:
-      return 0.3
+      return 0.2
 
   if torque_delta > 0.50:
-    return 0.04
+    return 0.02
   elif torque_delta > 0.20:
-    return 0.1
+    return 0.05
   elif torque_delta > 0.05:
-    return 0.2
+    return 0.1
   else:
-    return 0.3
+    return 0.2
 
 
 class CarController(CarControllerBase, MadsCarController, GasInterceptorCarController, IntelligentCruiseButtonManagementInterface):
