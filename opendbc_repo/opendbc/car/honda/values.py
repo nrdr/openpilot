0ace38d89cf6f0be30fb95c7de0d499727652ac1
nrdr <81265253+nrdr@users.noreply.github.com>
2026-05-16 19:06:43 -0400

    Defaults

--- opendbc_repo/opendbc/car/honda/values.py
+++ opendbc_repo/opendbc/car/honda/values.py
@@ -36,8 +36,8 @@ class CarControllerParams:
   BOSCH_GAS_LOOKUP_V = [0, 1600]
 
   STEER_STEP = 1  # 100 Hz
-  STEER_DELTA_UP = 0.8
-  STEER_DELTA_DOWN = 100
+  STEER_DELTA_UP = 15
+  STEER_DELTA_DOWN = 25
   STEER_GLOBAL_MIN_SPEED = 0 * CV.MPH_TO_MS
 
   def __init__(self, CP):
