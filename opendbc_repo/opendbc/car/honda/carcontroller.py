b0b81e14397fcd8ac927ee2eaf0880aaf08ce3a0
nrdr <81265253+nrdr@users.noreply.github.com>
2026-05-20 11:38:36 -0400

    Revert "Lateral: force fast LPF (tau=0.02) at high steering angles (>70°) ⚡"
    This reverts commit c200ddddb46bee0419790f904d9c46908a5b03e5.

--- opendbc_repo/opendbc/car/honda/carcontroller.py
+++ opendbc_repo/opendbc/car/honda/carcontroller.py
@@ -99,10 +99,7 @@ def process_hud_alert(hud_alert):
   return alert_fcw, alert_steer_required
 
 
-def _torque_lpf_tau(torque_cmd: float, prev_torque_cmd: float, v_ego: float, steering_angle_deg: float) -> float:
-  if abs(float(steering_angle_deg)) > 70.0:
-    return 0.02
-
+def _torque_lpf_tau(torque_cmd: float, prev_torque_cmd: float, v_ego: float) -> float:
   torque_delta = abs(float(torque_cmd) - float(prev_torque_cmd))
   sign_change = (float(torque_cmd) * float(prev_torque_cmd)) < 0.0
   highway = v_ego > 50.0 * CV.MPH_TO_MS
@@ -278,7 +275,7 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
       else:
         self.rejoin_ramp = min(1.0, self.rejoin_ramp + DT_CTRL / 3.0)
 
-        tau = _torque_lpf_tau(torque_cmd, self.prev_torque_cmd, CS.out.vEgo, CS.out.steeringAngleDeg)
+        tau = _torque_lpf_tau(torque_cmd, self.prev_torque_cmd, CS.out.vEgo)
         alpha = DT_CTRL / (tau + DT_CTRL)
 
         self.torque_lpf = alpha * torque_cmd + (1.0 - alpha) * self.torque_lpf
