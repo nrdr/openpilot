2f4d00dc6f17aa28daefee4114b4a90f92c29470
nrdr <81265253+nrdr@users.noreply.github.com>
2026-05-23 09:53:07 -0400

    Welcome CRV to the family!

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -399,11 +399,13 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.HONDA_CRV_5G:
       if ret.flags & HondaFlagsSP.EPS_MODIFIED:
-        # stock request input values:     0x0000, 0x00DB, 0x01BB, 0x0296, 0x0377, 0x0454, 0x0532, 0x0610, 0x067F
-        # stock request output values:    0x0000, 0x0500, 0x0A15, 0x0E6D, 0x1100, 0x1200, 0x129A, 0x134D, 0x1400
-        # modified request output values: 0x0000, 0x0500, 0x0A15, 0x0E6D, 0x1100, 0x1200, 0x1ACD, 0x239A, 0x2800
-        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2560, 10000], [0, 2560, 3840]]
-        CarInterfaceBase.configure_torque_tune(candidate, stock_cp.lateralTuning)
+        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 3840], [0, 3840]]
+        stock_cp.steerAtStandstill = True
+        stock_cp.minEnableSpeed = -1.0
+        stock_cp.minSteerSpeed = -1.0
+        stock_cp.lateralTuning.pid.kpBP, stock_cp.lateralTuning.pid.kpV = [[0, 10], [0.012, 0.12]]
+        stock_cp.lateralTuning.pid.kiBP, stock_cp.lateralTuning.pid.kiV = [[0, 10], [0.004, 0.04]]
+        stock_cp.lateralTuning.pid.kf = 0.000024
 
     elif candidate in (CAR.ACURA_MDX_3G, CAR.ACURA_MDX_3G_MMR):  # source mlocoteta
       stock_cp.autoResumeSng = True
