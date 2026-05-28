cce3ad4957f09f6f9937295ed02a259127a17532
nrdr <81265253+nrdr@users.noreply.github.com>
2026-05-28 17:04:45 -0400

    Change to fetch

--- selfdrive/ui/layouts/home.py
+++ selfdrive/ui/layouts/home.py
@@ -64,7 +64,7 @@ class NrdrForkWidget(Widget):
       self._force_update_status_color = rl.Color(190, 68, 68, 255)
       return
 
-    self._force_update_status_text = "SYNCING WITH UPSTREAM"
+    self._force_update_status_text = "FETCHING UPSTREAM"
     self._force_update_status_color = rl.Color(58, 150, 90, 255)
 
     subprocess.Popen(
