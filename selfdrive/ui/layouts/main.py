788ce800feea4518510af20e81d29b2759009f82
nrdr <81265253+nrdr@users.noreply.github.com>
2026-05-28 16:43:01 -0400

    Let's just not use this import to begin with

--- selfdrive/ui/layouts/main.py
+++ selfdrive/ui/layouts/main.py
@@ -5,16 +5,14 @@ from openpilot.system.ui.lib.application import gui_app
 from openpilot.selfdrive.ui.layouts.sidebar import Sidebar, SIDEBAR_WIDTH
 from openpilot.selfdrive.ui.layouts.home import HomeLayout
 from openpilot.selfdrive.ui.layouts.settings.settings import SettingsLayout, PanelType
+import openpilot.selfdrive.ui.layouts.settings.settings as OP
 from openpilot.selfdrive.ui.onroad.augmented_road_view import AugmentedRoadView
 from openpilot.selfdrive.ui.ui_state import device, ui_state
 from openpilot.system.ui.widgets import Widget
 from openpilot.selfdrive.ui.layouts.onboarding import OnboardingWindow
 
-from openpilot.selfdrive.ui.layouts.settings.settings import SettingsLayout, PanelType
-
 if gui_app.sunnypilot_ui():
   from openpilot.selfdrive.ui.sunnypilot.layouts.settings.settings import SettingsLayoutSP as SettingsLayout
-  from openpilot.selfdrive.ui.sunnypilot.layouts.settings.settings import PanelType
 
 
 class MainState(IntEnum):
@@ -34,7 +32,11 @@ class MainLayout(Widget):
     self._prev_onroad = False
 
     # Initialize layouts
-    self._layouts = {MainState.HOME: HomeLayout(), MainState.SETTINGS: SettingsLayout(), MainState.ONROAD: AugmentedRoadView()}
+    self._layouts = {
+      MainState.HOME: HomeLayout(),
+      MainState.SETTINGS: SettingsLayout(),
+      MainState.ONROAD: AugmentedRoadView(),
+    }
 
     self._sidebar_rect = rl.Rectangle(0, 0, 0, 0)
     self._content_rect = rl.Rectangle(0, 0, 0, 0)
@@ -61,7 +63,7 @@ class MainLayout(Widget):
     )
 
     self._layouts[MainState.HOME].set_settings_callback(
-      lambda: self.open_settings(PanelType.NRDR)
+      lambda: self.open_settings(OP.PanelType.NRDR)
     )
 
     self._layouts[MainState.SETTINGS].set_callbacks(
@@ -77,15 +79,25 @@ class MainLayout(Widget):
     )
 
   def _update_layout_rects(self):
-    self._sidebar_rect = rl.Rectangle(self._rect.x, self._rect.y, SIDEBAR_WIDTH, self._rect.height)
+    self._sidebar_rect = rl.Rectangle(
+      self._rect.x,
+      self._rect.y,
+      SIDEBAR_WIDTH,
+      self._rect.height,
+    )
 
     x_offset = SIDEBAR_WIDTH if self._sidebar.is_visible else 0
-    self._content_rect = rl.Rectangle(self._rect.y + x_offset, self._rect.y, self._rect.width - x_offset, self._rect.height)
+
+    self._content_rect = rl.Rectangle(
+      self._rect.x + x_offset,
+      self._rect.y,
+      self._rect.width - x_offset,
+      self._rect.height,
+    )
 
   def _handle_onroad_transition(self):
     if ui_state.started != self._prev_onroad:
       self._prev_onroad = ui_state.started
-
       self._set_mode_for_state()
 
   def _set_mode_for_state(self):
@@ -93,7 +105,9 @@ class MainLayout(Widget):
       # Don't hide sidebar from interactive timeout
       if self._current_mode != MainState.ONROAD:
         self._sidebar.set_visible(False)
+
       self._set_current_layout(MainState.ONROAD)
+
     else:
       self._set_current_layout(MainState.HOME)
       self._sidebar.set_visible(True)
